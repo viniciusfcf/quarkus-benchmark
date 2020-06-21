07c72f1c2e2c9cdeb89cea7aada4f207c4a33f6c
Vinicius <vinicius.ferraz@gmail.com>
2020-06-20 23:59:50 -0300

    atualizando endpoints

--- jaxrs/src/main/java/com/github/viniciusfcf/quarkus/UserResource.java
+++ jaxrs/src/main/java/com/github/viniciusfcf/quarkus/UserResource.java
@@ -2,7 +2,9 @@ package com.github.viniciusfcf.quarkus;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
+import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -10,18 +12,44 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 @Path("/users")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class UserResource {
 
+    private static final Random RANDOM = new Random();
+
+    @ConfigProperty(name = "page.size", defaultValue = "20")
+    Integer pageSize;
+
+    @GET
+    @Path("hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GET
+    @Path("sleep")
+    public String sleep() throws InterruptedException {
+        //sleep beetween 1 and 2 seconds
+        Thread.sleep(RANDOM.nextInt(1000)+1000);
+        return "waked up";
+    }
+
     @GET
     public List<User> findAll() {
-        return Collections.emptyList();
+        int usersCount = 100_000;
+        int maxPage = usersCount / pageSize;
+        int pageIndex = RANDOM.nextInt(maxPage);
+		return User.findAll().page(pageIndex, pageSize).list();
     }
 
     @POST
+    @Transactional
     public void add(User user) {
+        user.persist();
     }
 
     
