36b4f43c372f8baf5ac8eb3c5c2c26ef801309e8
Vinicius <vinicius.ferraz@gmail.com>
2020-07-28 23:52:25 -0300

    remove junits

--- jaxrs/src/test/java/com/github/viniciusfcf/quarkus/NativeHelloResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.github.viniciusfcf.quarkus;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeHelloResourceIT extends HelloResourceTest {
-
-    // Execute the same tests but in native mode.
-}
